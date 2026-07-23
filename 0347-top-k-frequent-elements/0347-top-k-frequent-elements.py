cd53d04c9b257c73cb7a2e678517b2d631f163fb
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-07-23 19:15:49 +0530

    Time: 7 ms (81.49%), Space: 14 MB (87.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
+        #O(n log k)
         counter = {}
         for n in nums:
             counter[n] = 1 + counter.get(n,0)
@@ -15,3 +16,20 @@ class Solution(object):
             result.append(heapq.heappop(heap)[1])
 
         return result
+
+
+        # O(n log n)
+        # counter = {}
+        # for n in nums:
+        #     counter[n] = 1 + counter.get(n,0)
+        
+        # counterArr = []
+        # for n, c in counter.items():
+        #     counterArr.append([c,n])
+        # counterArr.sort()
+
+        # result = []
+        # while len(result) < k:
+        #     result.append(counterArr.pop()[1])
+
+        # return result
