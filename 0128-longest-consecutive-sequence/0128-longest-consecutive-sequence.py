0d4e935537776e21c32af2c357da64ab3d310ada
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-07-23 23:27:54 +0530

    Update 0128-longest-consecutive-sequence.py

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,13 +1,26 @@
 class Solution(object):
     def longestConsecutive(self, nums):
+        #O(n log n)
+        nums.sort()
         maxLen = 0
-        sett = set(nums)
-        for n in sett:
-            if n-1 not in sett:
+        for i in range(len(nums)-1):
+            if nums[i+1] == 1+nums[i]:
                 currLen = 1
-                start = n
-                while start+1 in sett:
-                    currLen +=1
-                    start += 1
+                while nums[i+1] == 1+nums[i]:
+                    currLen += 1
+                    i+=1
                 maxLen = max(maxLen, currLen)
         return maxLen
+                
+        # O(n)
+        # maxLen = 0
+        # sett = set(nums)
+        # for n in sett:
+        #     if n-1 not in sett:
+        #         currLen = 1
+        #         start = n
+        #         while start+1 in sett:
+        #             currLen +=1
+        #             start += 1
+        #         maxLen = max(maxLen, currLen)
+        # return maxLen
