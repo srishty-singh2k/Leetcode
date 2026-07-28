918b44ea6f675d109926976ca0d878337bd366c1
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-07-28 21:04:33 +0530

    Time: 22 ms (52.84%), Space: 14.2 MB (14.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        #TC=O(n log n) SC=O(2n)
-        return sorted(s) == sorted(t)
+        #TC=O(n logn) SC=O(2n)
+        #return sorted(s) == sorted(t)
 
-        # TC=O(n) SC=O(2n)
-        # if len(s) != len(t):
-        #     return False
-        # mapS = {}
-        # mapT = {}
-        # for i in range(len(s)):
-        #     mapS[s[i]] = 1+ mapS.get(s[i],0)
-        #     mapT[t[i]] = 1+ mapT.get(t[i],0)
-        # return mapS == mapT
+        # TC=O(n) SC=O(2k)  k=no of distinct char
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1+ mapS.get(s[i],0)
+            mapT[t[i]] = 1+ mapT.get(t[i],0)
+        return mapS == mapT
         
