42c4ebb27eac314bf7800907d57967e3c3188054
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-07-29 22:06:13 +0530

    Time: 0 ms (100%), Space: 13.3 MB (35.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        
-        #TC=O(n^2) SC=O(1)
+        #TC=O(n) SC=O(n)
+        comp = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if (nums[i]+nums[j])==target:
-                    return [i,j]
+            diff = target-nums[i]
+            if  diff in comp:
+                return [i,comp[diff]]
+            else:
+                comp[nums[i]]=i
+
+        #TC=O(n^2) SC=O(1)
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if (nums[i]+nums[j])==target:
+        #             return [i,j]
