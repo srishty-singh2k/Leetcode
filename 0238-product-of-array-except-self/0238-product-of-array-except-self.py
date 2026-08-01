b50f45f41d5e35d76d858474386ad6c2f35e79e5
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-08-01 14:31:35 +0530

    Time: 35 ms (40.6%), Space: 20.5 MB (35.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,28 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        pre = []
-        post =[]
-        curr = 1
+        #TC=O(2n) SC=O(n)
+        pre = [1]
+        for i in range(1,len(nums)):
+            pre.append(pre[i-1]*nums[i-1])
 
-        for i in range(len(nums)):
-            pre.append(curr)
-            curr *= nums[i]
-        curr = 1
+        post = 1
         for i in range(len(nums)-1,-1,-1):
-            post.insert(0, curr)
-            curr *= nums[i]
-        for i in range(len(nums)):
-            nums[i] = pre[i] * post[i]
-        return nums
+            pre[i]=pre[i]*post
+            post *= nums[i]
+
+        return pre
+
+        #TC=O(3n) SC=O(2n)
+        # pre = []
+        # post =[]
+        # curr = 1
+        # for i in range(len(nums)):
+        #     pre.append(curr)
+        #     curr *= nums[i]
+        # curr = 1
+        # for i in range(len(nums)-1,-1,-1):
+        #     post.insert(0, curr)
+        #     curr *= nums[i]
+        # for i in range(len(nums)):
+        #     nums[i] = pre[i] * post[i]
+        # return nums
