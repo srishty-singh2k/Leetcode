7ec8a39a2ac8343c93d7363acbbd95a56f7d982b
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-08-11 23:41:17 +0530

    Time: 138 ms (19.11%), Space: 19 MB (74.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -8,6 +8,16 @@ class Solution(object):
             preMin=min(preMin,i)
         return maxProfit
 
+        #TC=O(n) SC=O(1)
+        # l,r=0,1
+        # maxP=0
+        # while(r<len(prices)):
+        #     if prices[r]>prices[l]:
+        #         maxP = max(maxP,prices[r]-prices[l])
+        #     else:
+        #         l=r
+        #     r+=1
+        # return maxP
 
         #TC=O(n^2) SC=O(1)
         # maxProfit = 0
