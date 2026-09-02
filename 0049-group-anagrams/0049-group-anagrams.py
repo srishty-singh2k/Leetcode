d9c4bcb16763fc87b68d0c39ef119883c9fe568e
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-09-02 17:45:51 +0530

    Update 0049-group-anagrams.py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,19 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        sortedMap = defaultdict(list)
-        for str in strs:
-            sortedS = ''.join(sorted(str))
-            sortedMap[sortedS].append(str)
-        return sortedMap.values()
+        # m=no of strings, n=longest string
+        #TC=O(m*n) SC=O(m*n)
+        res = defaultdict(list)
+        for s in strs:
+            counter = [0]*26
+            for c in s:
+                counter[ord(c)-ord('a')] += 1
+            res[tuple(counter)].append(s)
+        return list(res.values())
+
+        #TC=O(m*n logn) SC=O(m*n)
+        # sortedMap = defaultdict(list)
+        # for str in strs:
+        #     sortedS = ''.join(sorted(str))
+        #     sortedMap[sortedS].append(str)
+        # return sortedMap.values()
+        
