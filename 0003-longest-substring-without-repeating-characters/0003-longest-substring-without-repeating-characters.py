5ff9b6ef0dfe61ca0ff2c3dafe687130d4bfe2c8
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-09-08 16:15:51 +0530

    Time: 284 ms (15.35%), Space: 19.9 MB (24.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,13 +1,32 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxSub = 1 if len(s)>0 else 0
-        for i in range(len(s)):
-            sub = 0
-            seen = set()
-            j=i
-            while(j<len(s) and s[j] not in seen):
-                seen.add(s[j])
-                j+=1
-                sub+=1
-            maxSub=max(maxSub,sub)
+        if len(s)==0:
+            return 0
+        if len(s)==1:
+            return 1
+        maxSub = 1
+        l=0
+        r=1
+        seen=set(s[0])
+        while(l<len(s) and r<len(s)):
+            if s[r] in seen:
+                while(s[r] in seen):
+                    seen.remove(s[l])
+                    l+=1
+            maxSub = max(maxSub,r-l+1)
+            seen.add(s[r])
+            r+=1
         return maxSub
+
+
+        # TC=O(n*m) SC=O(m) m=No of unique ele
+        # for i in range(len(s)):
+        #     sub = 0
+        #     seen = set()
+        #     j=i
+        #     while(j<len(s) and s[j] not in seen):
+        #         seen.add(s[j])
+        #         j+=1
+        #         sub+=1
+        #     maxSub=max(maxSub,sub)
+        # return maxSub
