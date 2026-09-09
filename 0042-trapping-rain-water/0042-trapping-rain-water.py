4463b74d6ce240595d08a99646778f305f3cd4bd
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2026-09-09 11:28:57 +0530

    Time: 8 ms (52.77%), Space: 21 MB (84.52%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,20 @@
-class Solution(object):
-    def trap(self, height):
-        #TC=O(2n) SC=O(n)
-        res=0
-        pre = []
-        maxPre = 0
-        for i in range(len(height)):
-            pre.append(maxPre)
-            maxPre=max(maxPre,height[i])
-        maxPost = 0
-        for i in range(len(height)-1,-1,-1):
-            water = min(pre[i],maxPost)-height[i]
-            if water>0:
-                res += water
-            maxPost = max(maxPost,height[i])
-        return res
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # TWO-POINTERS TC=O(n) SC=O(1)
+        if len(height)<=2:
+            return 0
+        l=0
+        r=len(height)-1
+        maxL=height[l]
+        maxR=height[r]
+        water = 0
+        while(l<r):
+            if height[l]<=height[r]:
+                l+=1
+                water += (maxL-height[l]) if (maxL>height[l]) else 0
+                maxL = max(maxL,height[l])
+            else:
+                r-=1
+                water += (maxR-height[r]) if (maxR>height[r]) else 0 
+                maxR = max(maxR,height[r])
+        return water
